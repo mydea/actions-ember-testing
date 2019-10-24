d70a0a0e352bcf13291a05d6fa9ac23320dad9e8
Francesco Novy <francesco@cropster.com>
2019-10-24 11:54:54 +0200

    Initial implementation of action

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-actions-ember-testing
+# actions-ember-testing
+
+Sets up Node, Yarn, as well as problem matchers for ember tests to get a nicer output for errors.
+
+It uses [Volta](https://volta.sh/) to install Node & Yarn. 
+If you have custom Volta dependencies defined, it will use them as usual.
+
+## Inputs
+
+### `node-version`
+
+The node version to use. Defaults to `'10'`. Set to `''` to avoid installing it.
+
+### `yarn-version`
+
+The yarn version to use. Defaults to `'latest'`. Set to `''`  to avoid installing it.
+
+
+## Example usage
+
+```
+uses: mydea/actions-ember-testing@v1
+with:
+  node-version: '12'
+```
--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-import * as core from '@actions/core';
-import * as path from 'path';
+const core = require('@actions/core');
+const path = require('path');
+const fs = require('fs');
+const {exec} = require('@actions/exec');
+const tc = require('@actions/tool-cache');
 
 async function run() {
     try {
-        //
-        // Version is optional.  If supplied, install / use from the tool cache
-        // If not supplied then task is still used to setup proxy, auth, etc...
-        //
-        let version = core.getInput('version');
-        if (!version) {
-            version = core.getInput('node-version');
+        // This can be configured for the action
+        let nodeVersion = core.getInput('node-version');
+        let yarnVersion = core.getInput('yarn-version');
+
+        // Install Volta
+        let voltaScriptPath = await tc.downloadTool('https://get.volta.sh');
+        await exec(`bash ${voltaScriptPath}`);
+
+        // Install Node & Yarn
+        if (nodeVersion) {
+            await exec(`/home/runner/.volta/volta install node@${nodeVersion}`);
         }
 
-        console.log(version);
+        if (yarnVersion) {
+            await exec(`/home/runner/.volta/volta install yarn@${yarnVersion}`);
+        }
 
+        // Add Volta bins to path
+        console.log(`##[add-path]/home/runner/.volta/bin`);
 
-        const matchersPath = path.join(__dirname, '..', '.github');
+        // Add problem matchers for nice output of eslint & tsc errors
+        const matchersPath = path.join(__dirname, '.github');
         console.log(`##[add-matcher]${path.join(matchersPath, 'tsc.json')}`);
         console.log(
             `##[add-matcher]${path.join(matchersPath, 'eslint-stylish.json')}`
-        );
-        console.log(
-            `##[add-matcher]${path.join(matchersPath, 'eslint-compact.json')}`
         );
     } catch (error) {
         core.setFailed(error.message);
